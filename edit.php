28bf7709fe5bd28469c636515ca76263dcfad610
KyDevtw <k3030789@gmail.com>
2021-05-07 14:16:13 +0800

    add edit infomation

--- edit.php
+++ edit.php
@@ -23,7 +23,7 @@ require_once('./db.inc.php');
         require_once './template/navbar.php';
         require_once './template/modal.php';
         ?>
-        
+
         <div class="container">
             <!-- HERO SECTION------------------------------------------------>
             <section class="py-5 bg-light">
@@ -36,54 +36,81 @@ require_once('./db.inc.php');
                     </div>
                 </div>
             </section>
+            <!-- BILLING ADDRESS-->
             <section class="py-5">
-                <!-- BILLING ADDRESS-->
-                <h2 class="h5 text-uppercase mb-4">活動資料</h2>
-                <div class="row">
-                    <div class="col-lg-8">
-                        <form name="myForm" method="POST" action="./updateEdit.php" enctype="multipart/form-data">
-                            <div class="row">
-                                <div class="col-lg-6 form-group">
-                                    <label class="text-small text-uppercase" for="firstName">活動名稱</label>
-                                    <input class="form-control form-control-lg" id="eventName" name="eventName" type="text" placeholder="輸入名稱">
-                                </div>
-                                <div class="col-lg-6 form-group">
-                                    <label class="text-small text-uppercase" for="lastName">活動類別</label>
-                                    <input class="form-control form-control-lg" id="eventClass" name="eventClass" type="text" placeholder="類別C/D">
-                                </div>
-                                <div class="col-lg-6 form-group">
-                                    <label class="text-small text-uppercase">活動編號</label>
-                                    <input class="form-control form-control-lg" id="eventId" name="eventId" type="text" placeholder="三碼編號，展覽0開頭，工作坊1開頭">
-                                </div>
-                                <div class="col-lg-6 form-group">
-                                    <label class="text-small text-uppercase" for="phone">活動描述</label>
-                                    <input class="form-control form-control-lg" id="eventDescription" name="eventDescription" type="text" placeholder="描述活動">
-                                </div>
-                                <div class="col-lg-6 form-group">
-                                    <label class="text-small text-uppercase" for="company">開始日期</label>
-                                    <input class="form-control form-control-lg" id="eventDateStart" name="eventDateStart" type=" text" placeholder="yyyy-mm-dd">
-                                </div>
-                                <div class="col-lg-6 form-group">
-                                    <label class="text-small text-uppercase" for="company">結束日期</label>
-                                    <input class="form-control form-control-lg" id="eventDateEnd" name="eventDateEnd" type=" text" placeholder="yyyy-mm-dd">
-                                </div>
-                                <div class="col-lg-12 form-group">
-                                    <label class="text-small text-uppercase" for="address">活動票價</label>
-                                    <input class="form-control form-control-lg" id="eventPrice" name="eventPrice" type="text" placeholder="輸入整數值不含字元">
-                                </div>
-                                <div class="col-lg-12 form-group">
-                                    <label class="text-small text-uppercase" for="address">上傳圖片</label>
-                                    <input class="form-control form-control-lg" type="file" name="eventImg">
-                                </div>
+                <?php
+                // SQL 敘述
+                $sql = "SELECT `eventName`, `eventClass`, `eventId`, `eventDescription`,`eventDateStart`,`eventDateEnd`, `eventPrice`
+                    FROM `event` 
+                    WHERE `id` = ? ";
+                // 設定繫結值
+                $arrParam = [
+                    (int)$_GET['id']
+                ];
+                // 查詢
+                $stmt = $pdo->prepare($sql);
+                $stmt->execute($arrParam);
+                if ($stmt->rowCount() > 0) {
+                    $arr = $stmt->fetchAll()[0];
+                ?>
+                    <h2 class="h5 text-uppercase mb-4">活動資料</h2>
+                    <div class="row">
+                        <div class="col-lg-8">
+                            <form name="myForm" method="POST" action="./updateEdit.php" enctype="multipart/form-data">
+                                <div class="row">
+                                    <div class="col-lg-6 form-group">
+                                        <label class="text-small text-uppercase" for="firstName">活動名稱</label>
+                                        <input class="form-control form-control-lg" id="eventName" name="eventName" type="text" value="<?php echo $arr['eventName'] ?>">
+                                    </div>
+                                    <div class="col-lg-6 form-group">
+                                        <label class="text-small text-uppercase" for="lastName">活動類別</label>
+                                        <input class="form-control form-control-lg" id="eventClass" name="eventClass" type="text" value="<?php echo $arr['eventClass'] ?>" maxlength="10">
+                                    </div>
+                                    <div class="col-lg-6 form-group">
+                                        <label class="text-small text-uppercase">活動編號</label>
+                                        <input class="form-control form-control-lg" id="eventId" name="eventId" type="text" value="<?php echo $arr['eventId'] ?>">
+                                    </div>
+                                    <div class="col-lg-6 form-group">
+                                        <label class="text-small text-uppercase" for="phone">活動描述</label>
+                                        <input class="form-control form-control-lg" id="eventDescription" name="eventDescription" type="text" value="<?php echo $arr['eventDescription'] ?>">
+                                    </div>
+                                    <div class="col-lg-6 form-group">
+                                        <label class="text-small text-uppercase" for="company">開始日期</label>
+                                        <input class="form-control form-control-lg" id="eventDateStart" name="eventDateStart" type=" text" value="<?php echo $arr['eventDateStart'] ?>">
+                                    </div>
+                                    <div class="col-lg-6 form-group">
+                                        <label class="text-small text-uppercase" for="company">結束日期</label>
+                                        <input class="form-control form-control-lg" id="eventDateEnd" name="eventDateEnd" type=" text" value="<?php echo $arr['eventDateEnd'] ?>">
+                                    </div>
+                                    <div class="col-lg-12 form-group">
+                                        <label class="text-small text-uppercase" for="address">活動票價</label>
+                                        <input class="form-control form-control-lg" id="eventPrice" name="eventPrice" type="text" value="<?php echo $arr['eventPrice'] ?>">
+                                    </div>
+                                    <div class=" col-lg-12 form-group">
+                                        <label class="text-small text-uppercase" for="address">上傳圖片</label>
+                                        <?php if ($arr['eventImg'] !== NULL) { ?>
+                                            <img class="w200px" src="./images/<?php echo $arr['eventImg'] ?>" />
+                                        <?php } ?>
+                                        <input class="form-control form-control-lg" type="file" name="eventImg">
+                                    </div>
 
-                                <div class="col-lg-12 form-group">
-                                    <input type="hidden" name="id" value="<?php echo (int)$_GET['id'] ?>">
-                                    <input class="btn btn-dark" type="submit" name="smb" value="建立">
+                                    <div class="col-lg-12 form-group">
+                                        <input type="hidden" name="id" value="<?php echo (int)$_GET['id'] ?>">
+                                        <input class="btn btn-dark" type="submit" name="smb" value="建立">
+                                    </div>
                                 </div>
-                            </div>
-                        </form>
+                            </form>
+                        </div>
                     </div>
-                </div>
+                <?php
+                } else {
+                ?>
+                    <tr>
+                        <td class="border" colspan="6">沒有資料</td>
+                    </tr>
+                <?php
+                }
+                ?>
             </section>
         </div>
         <footer class="bg-dark text-white">
